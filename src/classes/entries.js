62177f9bbf422e0e96e7261f8fa04072a837c6dd
branmar97 <brandonmar97@gmail.com>
2021-01-12 14:58:43 -0500

    Add entry form bindings to Entries class

--- src/classes/entries.js
+++ src/classes/entries.js
@@ -2,4 +2,12 @@ class Entries {
     constructor() {
         this.entries = [];
     }
+
+    newEntryBindings() {
+        this.newEntryForm = document.getElementById("new-entry-form")
+        this.newEntryTitle = document.getElementById("new-entry-title")
+        this.newEntryAuthor = document.getElementById("new-entry-author")
+        this.newEntryText = document.getElementById("new-entry-text")
+        this.newEntryForm.addEventListener('submit', this.createNewEntry.bind(this));
+    }
 }
