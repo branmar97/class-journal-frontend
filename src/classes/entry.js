8f865a13fb5631f586c7fdb5ffa730c8198f365a
branmar97 <brandonmar97@gmail.com>
2021-01-12 15:20:28 -0500

    Build entry constructor method

--- src/classes/entry.js
+++ src/classes/entry.js
@@ -1,4 +1,12 @@
 class Entry {
+    constructor(entry) {
+        this.id = entry.id
+        this.title = entry.title
+        this.author = entry.author 
+        this.text = entry.text
+        this.comments = entry.comments
+    }
+
     renderEntry() {
         const entriesContainer = document.getElementById("entries-container")
     }
