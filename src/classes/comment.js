d699de35457853d5c1eb96c1b2e1ac8aee3b937d
branmar97 <brandonmar97@gmail.com>
2021-01-12 16:58:36 -0500

    Create comment class

---
+++ src/classes/comment.js
@@ -0,0 +1,8 @@
+class Comment {
+    constructor(comment) {
+        this.id = comment.id 
+        this.author = comment.author
+        this.text = comment.text 
+        this.entryId = comment.entry_id
+    }
+}
