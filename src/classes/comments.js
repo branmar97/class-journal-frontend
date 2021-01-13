e0e161b770085d45ff5fede942a7cca4c503c1ce
branmar97 <brandonmar97@gmail.com>
2021-01-13 10:32:25 -0500

    Add method to fetch and create comments

--- src/classes/comments.js
+++ src/classes/comments.js
@@ -4,4 +4,14 @@ class Comments {
         this.adapter = new ApiAdapter()
         this.fetchAndLoadComments()
     }
+
+    fetchAndLoadComments() {
+        this.adapter.getComments()
+        .then(comments => {
+            comments.forEach(comment => this.comments.push(new Comment(comment)))
+        })
+        .then(() => {
+            this.renderComments()
+        })
+    }
 }
