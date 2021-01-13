831d42ee9305ef3ad09c84a265d3c0838ece35c1
branmar97 <brandonmar97@gmail.com>
2021-01-13 10:06:08 -0500

    Create renderComment method and add comment container

--- src/classes/comment.js
+++ src/classes/comment.js
@@ -5,4 +5,14 @@ class Comment {
         this.text = comment.text 
         this.entryId = comment.entry_id
     }
+
+    renderComment() {
+        const entryCommentsDiv = document.getElementById(`entry-${this.entryId}-comment-container`)
+
+        // Comment Container
+        const commentDiv = document.createElement("div")
+        commentDiv.className = "entry-comment"
+        commentDiv.id = `entry-comment-${this.id}`
+        entryCommentsDiv.appendChild(commentDiv)
+    }
 }
